9ac72892282a85698683664c9ed28a408afadf7b
Felix Isaac Palomares <felixisaacp@gmail.com>
2012-01-16 20:08:51 +0800

    New solution utilizing a binary search tree

--- src/substr.c
+++ src/substr.c
@@ -13,28 +13,37 @@ struct node {
 };
 
 struct node* substr (char** str, int num);
-void insert (struct node** current, struct node* newNode);
-void search (struct node* current, int* ctr, int k);
+int insert (struct node** head, struct node* newNode);
+void search (struct node* current, unsigned long long* ctr, unsigned long long k);
+void clear (struct node* head);
 
 int main () {
 	char buff[BUFFER_SIZE];
 	struct node* list = NULL;
 	char** str;
-	int k[MAX_QUERY];
+	unsigned long long k[MAX_QUERY];
+	unsigned long long len;
 	int n, q, x;
-	int len, maxcount;
 
 	fgets (buff, BUFFER_SIZE, stdin);
 	n = atoi (buff);
 	if (n < 1 || n > MAX_STRING) {
 		fprintf (stderr, "Number of strings should be > 0 and < MAX_STRING\n");
-		return (EXIT_FAILURE);
+		return (1);
 	}
 
-	str = (char**) malloc (MAX_STRING * sizeof (char*));
+	str = (char**) malloc (n * sizeof (char*));
+	if (str == NULL) {
+		fprintf (stderr, "No memory allocated for str\n");
+		return (1);
+	}
 	for (x = 0; x < n; x++) {
-		str[x] = (char*) malloc (MAX_STRING * sizeof (char*));
-		fgets (buff, BUFFER_SIZE, stdin);
+		str[x] = (char*) malloc (BUFFER_SIZE * sizeof (str[x]));
+		if (str[x] == NULL) {
+			fprintf (stderr, "No memory allocated for str[x]\n");
+			return (1);
+		}
+		fgets (buff, BUFFER_SIZE - 1, stdin);
 		len = strlen (buff);
 		if (buff[len - 1] == '\n')
 			buff[len - 1] = '\0';
@@ -45,22 +54,15 @@ int main () {
 	q = atoi (buff);
 	if (q < 1 || q > MAX_QUERY) {
 		fprintf (stderr, "Number of queries should be > 0 and < MAX_QUERY\n");
-		return (EXIT_FAILURE);
+		return (1);
 	}
 
 	for (x = 0; x < q; x++) {
 		fgets (buff, BUFFER_SIZE, stdin);
 		k[x] = atoi (buff);
 
-		if (k[x] < 1 || k[x] > 1000000000) {
+		if (k[x] < 1 || k[x] > 1000000000)
 			fprintf (stderr, "Query should be > 0 and < 1000000000\n");
-		}
-	}
-
-	maxcount = 0;
-	for (x = 0; x < n; x++) {
-		len = strlen (str[x]);
-		maxcount += (len * (len + 1)) / 2;
 	}
 
 	list = substr (str, n);
@@ -68,70 +70,88 @@ int main () {
 	for (x = 0; x < q; x++) {
 		len = 0;
 		search (list, &len, k[x]);
-		
+
 		if (len < k[x])
 			printf ("INVALID\n");
 	}
 
-	return (EXIT_SUCCESS);
+	clear (list);
+	
+	for (x = 0; x < n; x++)
+		free (str[x]);
+		
+	free (str);
+
+	return (0);
 }
 
 // Create all the possible substrings from the input
 struct node* substr (char** str, int num) {
 	struct node* head = NULL;
-	struct node* newNode = NULL;
+	struct node* newNode;
 	int start, end;
 	int len;
 	int x;
 
-	start = 0;
-	end = 1;
-	x = 0;
-	len = strlen (str[x]);
-
 	// get the string pointed to from start to end
 	// increment end by 1
 	// increment start by 1 when the end pointer has reached the end of string
-	while (1) {
-		newNode = (struct node*) malloc (sizeof (struct node));
-		strncpy (newNode->str, str[x] + start, end);
-		newNode->left = NULL;
-		newNode->right = NULL;
+	for (x = 0; x < num; x++) {
+		len = strlen (str[x]);
+		for (start = 0; start < len; start++) {
+			for (end = start + 1; end <= len; end++) {
+				newNode = (struct node*) malloc (sizeof (struct node));
+				if (newNode == NULL) {
+					fprintf (stderr, "No memory allocated for newNode\n");
+					exit (1);
+				}
+				strncpy (newNode->str, str[x] + start, end);
+				newNode->str[end - start] = '\0';
+				newNode->left = NULL;
+				newNode->right = NULL;
 
-		insert (&head, newNode);
-		end++;
-
-		if (end + start > len) {
-			start++;
-			end = 1;
-		}
-
-		if (start == len) {
-			start = 0;
-			end = 1;
-			x++;
-			if (x == num)
-				break;
-			len = strlen (str[x]);
+				if (insert (&head, newNode) == 1)
+					free (newNode);
+			}
 		}
 	}
 
 	return (head);
 }
 
-// Insert into the tree in alphabetical order
-void insert (struct node** current, struct node* newNode) {
-	if (*current == NULL)
-		(*current) = newNode;
-	else if (strcmp (newNode->str, (*current)->str) == 0)
-		free (newNode);
-	else if (strcmp (newNode->str, (*current)->str) < 0)
-		insert (&((*current)->left), newNode);
-	else if (strcmp (newNode->str, (*current)->str) > 0)
-		insert (&((*current)->right), newNode);
+// Insert into the binary tree
+int insert (struct node** head, struct node* newNode) {
+	struct node* current;
+	struct node* prev;
+	int res;
+	current = (*head);
+
+	while (current != NULL) {
+		res = strcmp (newNode->str, current->str);
+		prev = current;
+
+		if (res == 0) {
+			return (1);
+		}
+		else if (res < 0)
+			current = current->left;
+		else
+			current = current->right;
+	}
+
+	if ((*head) == NULL) 
+		(*head) = newNode;
+	else if (current == NULL) {
+		if (res < 0)
+			prev->left = newNode;
+		else
+			prev->right = newNode;
+	}
+
+	return (0);
 }
 
-void search (struct node* current, int* ctr, int k) {
+void search (struct node* current, unsigned long long* ctr, unsigned long long k) {
 	if (current == NULL)
 		return;
 
@@ -145,4 +165,13 @@ void search (struct node* current, int* ctr, int k) {
 		if ((*ctr) <= k)
 			search (current->right, ctr, k);
 	}
+}
+
+void clear (struct node* head) {
+	if (head == NULL)
+		return;
+
+	clear (head->left);
+	clear (head->right);
+	free (head);
 }
